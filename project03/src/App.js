6c830ed69ddb197c25850f09fc0bde2cf5d56720
임아해 <limahae1015@gmail.com>
2022-10-11 16:13:41 +0900

    날씨 api 가져오기!

--- project03/src/App.js
+++ project03/src/App.js
@@ -9,6 +9,7 @@ import Ex07 from './Example/Ex07';
 import Ex08 from './Example/Ex08';
 import Ex09 from './Example/Ex09';
 import Ex06B from './Example/Ex06B';
+import Ex10 from './Example/Ex10';
 function App() {
   
   return (
@@ -33,7 +34,7 @@ function App() {
       {/* <Ex06/> */}
       
       {/* ex06-2 input 값 추출하기 */}
-      <Ex06B/>
+      {/* <Ex06B/> */}
 
       {/* ex07 : 클래스 컴포넌트 */}
       {/* <Ex07/> */}
@@ -43,6 +44,9 @@ function App() {
 
       {/* ex09 : 영화데이터 리액트로 바꾸기! */}
       {/* <Ex09/> */}
+
+      {/* ex10 : 날씨 open API */}
+      <Ex10/>
 
     </div>
   );
---
+++ project03/src/Example/Ex10.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const Ex10 = () => {
+    //렌더링 이후(useEffect) 현재 위경도를 구하는 메서드 실행되고 그 값으로 날씨데이터를 가져옴
+
+    let url = ''
+
+    const [city, setCity] = useState('')
+    const [des, setDes] = useState('')
+    const [temp, setTemp] = useState(0)
+
+    const getCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position)=>{
+            // console.log(position)
+            console.log('현 위치의 위도 : ',position.coords.latitude)
+            console.log('현 위치의 경도 : ',position.coords.longitude)
+            
+            // 내 위치를 구한 이후 => 날씨 데이터 요청
+            getWeatherData(position.coords.latitude, position.coords.longitude)
+        })
+    }
+
+    const getWeatherData = (위도, 경도) => {
+        console.log(위도, 경도)
+        url = `https://api.openweathermap.org/data/2.5/weather?lat=${위도}&lon=${경도}&appid=d98ae2c3057b1f588501393dc077fc5f&units=metric`
+        
+        axios.get(url)
+            .then((res)=>{
+                console.log(res.data)
+
+                setCity(res.data.name)
+                setDes(res.data.weather[0].description)
+                setTemp(res.data.main.temp)
+                
+            })
+            .catch(()=>{alert('error!')})
+    
+    }
+
+    useEffect(()=>{
+        getCurrentLocation()
+    },[])
+  return (
+    <div>
+        <h1>{city}</h1>
+        <p>{des}</p>
+        <p>현재 온도 : {temp}°C</p>        
+    </div>
+  )
+}
+
+export default Ex10
